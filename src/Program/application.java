b0d74cd79f482cab5559914b87f4fb12a694e528
ivanpst <72033769+ivanpst@users.noreply.github.com>
2021-08-10 11:52:24 -0300

    Department entity class

---
+++ src/Program/application.java
@@ -0,0 +1,14 @@
+package Program;
+
+import model.entities.Department;
+
+public class application {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+
+	}
+
+}
