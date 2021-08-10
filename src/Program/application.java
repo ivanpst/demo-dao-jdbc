e2becb1fbc43198bed2b160d28dce22a0e10c661
ivanpst <72033769+ivanpst@users.noreply.github.com>
2021-08-10 13:40:23 -0300

    Seller entites class

--- src/Program/application.java
+++ src/Program/application.java
@@ -1,13 +1,19 @@
 package Program;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class application {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "bob", "Bob@gmail.com", new Date(), 3000.0, obj) ;
+		
+		System.out.println(seller);
 
 	}
 
